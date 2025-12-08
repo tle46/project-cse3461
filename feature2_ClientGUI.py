7a12cec7aea901332f2ed6479f151a5e5e21a660
Maya Williams <110641558+mayaaleahanne@users.noreply.github.com>
2025-12-08 18:31:49 -0500

    Update and rename Feature2ClientGUI.py to feature2_ClientGUI.py

--- feature2_ClientGUI.py
+++ feature2_ClientGUI.py
@@ -1,9 +1,10 @@
 import tkinter as tk
-from Feature2GUIClasses import ClientGUI
+from feature2_GUIClasses import ClientGUI
 
 # Run the client GUI application
 root = tk.Tk()
 app = ClientGUI(root)
 root.mainloop()
+
 
 
