1d745768b743c07633ce37797051f41f954539f9
Maya Williams <110641558+mayaaleahanne@users.noreply.github.com>
2025-12-08 16:57:36 -0500

    Update Feature2ClientGUI.py

--- Feature2ClientGUI.py
+++ Feature2ClientGUI.py
@@ -1,7 +1,8 @@
 import tkinter as tk
-from ClientClass import ClientGUI
+from GUIClasses import ClientGUI
 
 # Run the client GUI application
 root = tk.Tk()
 app = ClientGUI(root)
 root.mainloop()
+
